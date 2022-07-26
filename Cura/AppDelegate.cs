3e557e56251d21b6fdbca5545bbc363f80607c1b
Mustafa <mustafa_7070@yahoo.com>
2022-07-26 02:01:01 +0200

    Project architecture

---
+++ Cura/AppDelegate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace Cura
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+
+        public UIWindow window { get; set; }
+
+      //  public IntPtr Handle => throw new NotImplementedException();
+
+        UICollectionViewController homeCollectionViewController;
+        UICollectionViewFlowLayout flowLayout;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var _width = window.Bounds.Width;
+
+            // Flow Layout
+            flowLayout = new UICollectionViewFlowLayout()
+            {
+                ItemSize = new CGSize(_width, 300),
+                SectionInset = new UIEdgeInsets(20, 20, 20, 20),
+                ScrollDirection = UICollectionViewScrollDirection.Vertical,
+                MinimumInteritemSpacing = 50, // minimum spacing between cells
+                MinimumLineSpacing = 50 // minimum spacing between rows if ScrollDirection is Vertical or between columns if Horizontal
+            };
+
+            homeCollectionViewController = new HomeViewController(flowLayout);
+
+            //homeCollectionViewController.CollectionView.ContentInset = new UIEdgeInsets(50, 0, 0, 0);
+
+            window.RootViewController = homeCollectionViewController;
+            window.MakeKeyAndVisible();
+
+            return true;
+        }
+
+        //public void Dispose()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+
---
+++ Cura/HomeViewController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreGraphics;
+using Cura.Cells;
+using Cura.Models;
+using Foundation;
+using UIKit;
+
+namespace Cura
+{
+    public class HomeViewController : UICollectionViewController//, IUISearchResultsUpdating, IUICollectionViewDelegateFlowLayout
+    {
+        //Home
+        public List<ISection> HomeList { get; set; } = new List<ISection>();
+        static NSString verticalCellId = new NSString("VerticalCollectionViewCell");
+
+        public HomeViewController(UICollectionViewLayout layout) : base(layout)
+        {
+            FillHomeList();
+        }
+
+        
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            
+
+
+            //CollectionView.BackgroundColor = UIColor.Yellow;
+
+            CollectionView.RegisterClassForCell(typeof(VerticalCollectionViewCell), verticalCellId);
+
+
+        }
+
+
+        #region Override Methods
+
+        public override nint NumberOfSections(UICollectionView collectionView)
+        {
+            // We only have one section
+            return 1;
+        }
+
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            // Return the number of items
+            return HomeList.Count;
+        }
+
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            // Get a reusable cell and set {~~it's~>its~~} title from the item
+            var cell = collectionView.DequeueReusableCell(verticalCellId, indexPath) as VerticalCollectionViewCell;
+            var section = HomeList[(int)indexPath.Item];
+            if (section is ReviewSection reviewSection)
+            {
+                cell.SetReviewCell(reviewSection);
+            }
+            else if (section is DoctorSection doctorSection)
+            {
+                cell.SetDoctorCell(doctorSection);
+            }
+
+            return cell;
+        }
+
+        //public override CGSize GetSizeForChildContentContainer(IUIContentContainer contentContainer, CGSize parentContainerSize)
+        //{
+        //    return base.GetSizeForChildContentContainer(contentContainer, parentContainerSize);
+        //}
+
+        //[Export("collectionView:layout:sizeForItemAtIndexPath:")]
+        //public CGSize GetSizeForItem(UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+        //{
+        //    return new CGSize(width: View.Frame.Width, height: 300);
+        //}
+
+        //[Export("collectionView:layout:minimumLineSpacingForSectionAtIndex:")]
+        //public nfloat GetMinimumLineSpacingForSection(UICollectionView collectionView, UICollectionViewLayout layout, nint section)
+        //{
+        //    return 0;
+        //}
+        #endregion
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+
+        private void FillHomeList()
+        {
+            ReviewSection reviewSection = new ReviewSection();
+            reviewSection.Title = "Our happy customers";
+            reviewSection.Reviews = new List<Review>();
+            for (int i = 0; i < 10; i++)
+            {
+                reviewSection.Reviews.Add(new Review());
+            }
+            HomeList.Add(reviewSection);
+
+            DoctorSection doctorSection = new DoctorSection();
+            doctorSection.Title = "Doctors accepting MedGulf insurance";
+            doctorSection.Doctors = new List<Doctor>();
+            for (int i = 0; i < 10; i++)
+            {
+                doctorSection.Doctors.Add(new Doctor());
+            }
+            HomeList.Add(doctorSection);
+
+            // add more sections here ...
+        }
+
+    }
+}
+
